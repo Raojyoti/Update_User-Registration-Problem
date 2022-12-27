3f5cac63138779296d104b7045472db5b395642d
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-27 19:52:37 +0530

    [Jyoti Rao] FirstName: Check validation for firstname

---
+++ User Registration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter the First Name: ");
+            string firstname = Console.ReadLine();
+            bool name = pat.Validate_FirstName(firstname);
+            Console.WriteLine(name);
+            
+        }
+    }
+}
